245458177613def7495a2fa41e9d40a45e0e4f8f
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2016-05-18 13:13:05 -0400

    Fix integration test for Firmament HEAD.
    FlowScheduler now takes a few more arguments. Also correctly processes
    command line flags and initialises logging now.
    
    Change-Id: I0633d94df208ec21d79763170b9930e903849211

--- src/firmament/test_integration.cc
+++ src/firmament/test_integration.cc
@@ -1,20 +1,26 @@
 #include "base/resource_topology_node_desc.pb.h"
 #include "misc/utils.h"
+#include "misc/trace_generator.h"
 #include "misc/wall_time.h"
 #include "platforms/sim/simulated_messaging_adapter.h"
 #include "scheduling/flow/flow_scheduler.h"
 #include "storage/stub_object_store.h"
+
+#include <glog/logging.h>
+#include <gflags/gflags.h>
 
 DEFINE_string(listen_uri, "", "");
 
 using boost::shared_ptr;
 using firmament::BaseMessage;
 using firmament::JobMap_t;
+using firmament::ResourceDescriptor;
 using firmament::ResourceMap_t;
 using firmament::ResourceID_t;
 using firmament::TaskMap_t;
 using firmament::KnowledgeBase;
 using firmament::ResourceTopologyNodeDescriptor;
+using firmament::TraceGenerator;
 using firmament::WallTime;
 using firmament::platform::sim::SimulatedMessagingAdapter;
 using firmament::scheduler::FlowScheduler;
@@ -29,6 +35,10 @@ shared_ptr<TaskMap_t> task_map_;
 shared_ptr<TopologyManager> topology_manager_;
 
 int main(int argc, char** argv) {
+
+  google::ParseCommandLineFlags(&argc, &argv, false);
+  google::InitGoogleLogging(argv[0]);
+
   job_map_.reset(new JobMap_t);
 
   ResourceID_t res_id = firmament::GenerateResourceID();
@@ -36,13 +46,16 @@ int main(int argc, char** argv) {
   ResourceTopologyNodeDescriptor rtnd;
   rtnd.mutable_resource_desc()->set_uuid(
     firmament::to_string(res_id));
+  rtnd.mutable_resource_desc()->set_type(
+      ResourceDescriptor::RESOURCE_COORDINATOR);
 
   SimulatedMessagingAdapter<BaseMessage> ma;
   WallTime wall_time;
+  TraceGenerator tg(&wall_time);
 
   FlowScheduler fs(job_map_, resource_map_, &rtnd, obj_store_,
                    task_map_, knowledge_base_, topology_manager_,
-                   &ma, NULL, res_id, "", &wall_time);
+                   &ma, NULL, res_id, "", &wall_time, &tg);
 
   LOG(INFO) << fs;
 }
