10cd124d184edc27e91106a49870c2e3eb4d9432
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2016-05-20 13:25:32 -0400

    Add GTest dependency.
    Some of the Firmament headers require the GTest headers to be
    available. We'll also want to use GTest going forward, so it should be a
    dependency.
    
    Change-Id: Iabf70488ab9cba12f0940dc5d45e4531a37801f8

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -36,7 +36,34 @@ ExternalProject_Add(
 ExternalProject_Get_Property(cpp-rest-sdk SOURCE_DIR)
 ExternalProject_Get_Property(cpp-rest-sdk BINARY_DIR)
 set(cpp-rest-sdk_SOURCE_DIR ${SOURCE_DIR})
+set(cpp-rest-sdk_INCLUDE_DIR ${SOURCE_DIR}/Release/include)
 set(cpp-rest-sdk_BINARY_DIR ${BINARY_DIR}/Binaries)
+
+# Add Google Test dependency
+ExternalProject_Add(
+    gtest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    TIMEOUT 10
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/third_party/gtest
+    # no install required, we link the library from the build tree
+    INSTALL_COMMAND ""
+    # Wrap download, configure and build steps in a script to log output
+    LOG_DOWNLOAD ON
+    LOG_BUILD ON)
+
+ExternalProject_Get_Property(gtest BINARY_DIR)
+ExternalProject_Get_Property(gtest SOURCE_DIR)
+set(gtest_BINARY_DIR ${BINARY_DIR})
+set(gtest_SOURCE_DIR ${SOURCE_DIR})
+set(gtest_INCLUDE_DIR ${gtest_SOURCE_DIR}/googletest/include)
+include_directories(${gtest_INCLUDE_DIR})
+set(gtest_LIBRARY ${gtest_BINARY_DIR}/googlemock/gtest/libgtest.a)
+set(gtest_MAIN_LIBRARY ${gtest_BINARY_DIR}/googlemock/gtest/libgtest_main.a)
+
+set(gmock_INCLUDE_DIR ${gtest_SOURCE_DIR}/googlemock/include)
+include_directories(${gmock_INCLUDE_DIR})
+set(gmock_LIBRARY ${gtest_BINARY_DIR}/googlemock/libgmock.a)
+set(gmock_MAIN_LIBRARY ${gtest_BINARY_DIR}/googlemock/libgmock_main.a)
 
 # Add Firmament as a dependency
 ExternalProject_Add(
