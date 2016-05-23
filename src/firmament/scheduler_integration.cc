ed699b8b4b00e680bc2e75ef1e1e2b9ea6cc8e61
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2016-05-23 01:45:45 -0400

    Create Firmament jobs for K8s pods, and schedule.
    This change generates a single-task Firmament job for each pending
    Kubernetes pod found, initializes the Firmament data structures
    appropritately, and invokes the scheduler. There is currently no way for
    the scheduler to communicate its decisions back, so the decisions aren't
    actually applied yet.
    
    Change-Id: I203788f46b5ca8171224a398664aafd9bdbb8ae8

--- src/firmament/scheduler_integration.cc
+++ src/firmament/scheduler_integration.cc
@@ -1,5 +1,6 @@
 #include "base/resource_status.h"
 #include "base/resource_topology_node_desc.pb.h"
+#include "misc/map-util.h"
 #include "misc/utils.h"
 #include "misc/trace_generator.h"
 #include "misc/wall_time.h"
@@ -16,11 +17,18 @@
 DEFINE_string(listen_uri, "", "");
 
 using firmament::BaseMessage;
+using firmament::ContainsKey;
+using firmament::FindOrNull;
+using firmament::InsertIfNotPresent;
+using firmament::JobID_t;
+using firmament::JobDescriptor;
 using firmament::JobMap_t;
 using firmament::ResourceDescriptor;
 using firmament::ResourceMap_t;
 using firmament::ResourceID_t;
 using firmament::ResourceStatus;
+using firmament::TaskID_t;
+using firmament::TaskDescriptor;
 using firmament::TaskMap_t;
 using firmament::KnowledgeBase;
 using firmament::ResourceTopologyNodeDescriptor;
@@ -39,6 +47,28 @@ boost::shared_ptr<ObjectStoreInterface> obj_store_;
 boost::shared_ptr<ResourceMap_t> resource_map_;
 boost::shared_ptr<TaskMap_t> task_map_;
 boost::shared_ptr<TopologyManager> topology_manager_;
+
+unordered_map<string, TaskID_t> pod_to_task_map_;
+
+JobDescriptor* CreateJobForPod(const string& pod) {
+  // Fake out a job for this pod
+  // XXX(malte): we should equate a Firmament "job" with a K8s
+  // "deployment" and "job" for a more sane notion here.
+  JobID_t job_id = firmament::GenerateJobID();
+  JobDescriptor new_jd;
+  CHECK(InsertIfNotPresent(job_map_.get(), job_id, new_jd));
+  JobDescriptor* jd = FindOrNull(*job_map_, job_id);
+  jd->set_uuid(firmament::to_string(job_id));
+  jd->set_name(pod);
+  jd->set_state(JobDescriptor::CREATED);
+  TaskDescriptor* root_td = jd->mutable_root_task();
+  root_td->set_uid(firmament::GenerateRootTaskID(*jd));
+  root_td->set_name(pod);
+  root_td->set_state(TaskDescriptor::CREATED);
+  root_td->set_job_id(jd->uuid());
+  CHECK(InsertIfNotPresent(task_map_.get(), root_td->uid(), root_td));
+  return jd;
+}
 
 ResourceStatus* CreateTopLevelResource(void) {
   ResourceID_t res_id = firmament::GenerateResourceID();
@@ -81,6 +111,7 @@ int main(int argc, char** argv) {
   K8sApiClient api_client;
 
   job_map_.reset(new JobMap_t);
+  task_map_.reset(new TaskMap_t);
   resource_map_.reset(new ResourceMap_t);
 
   ResourceStatus* toplevel_res_status = CreateTopLevelResource();
@@ -118,17 +149,33 @@ int main(int argc, char** argv) {
     // Poll pods
     vector<string> pods = api_client.AllPods();
     if (!pods.empty()) {
-      for (auto& p : pods) {
-        LOG(INFO) << "Pod: " << p;
-        // XXX(malte): test hack -- always bind to first node
-        // Note that this will try to re-bind even already bound pods at the
-        // moment.
-        if (!nodes.empty()) {
-          api_client.BindPodToNode(p, nodes[0].second);
-        }
+      vector<string>::iterator unscheduled_pods_iter =
+        find_if(pods.begin(), pods.end(),
+          [](const string& p) -> bool {
+            // TODO(malte): should use p.state() == "Pending" or similar
+            return firmament::FindOrNull(pod_to_task_map_, p) == NULL;
+          });
+      if (unscheduled_pods_iter != pods.end()) {
+        for_each (unscheduled_pods_iter, next(unscheduled_pods_iter, 1),
+          [&](string p) {
+            LOG(INFO) << "New unscheduled pod: " << p;
+            JobDescriptor* jd = CreateJobForPod(p);
+            CHECK(InsertIfNotPresent(&pod_to_task_map_, p,
+                                     jd->root_task().uid()));
+            fs.AddJob(jd);
+          }
+        );
       }
     }
 
+    // Invoke Firmament scheduling
+    firmament::scheduler::SchedulerStats sstat;
+    fs.ScheduleAllJobs(&sstat);
+
+    // Apply results
+    // XXX(malte): TODO
+
+    // Sleep a bit until we poll again
     sleep(10);
   }
 }
