54dfedd973f044a24c6af5569eeaf81d75be7264
Eissa Nematollahi <eissa.nematollahi1@huawei.com>
2017-03-14 03:08:42 +0000

    Sets node's hostname when creating its resource topology.
    Change-Id: I5b96b5a41c1783638b8d904e05164a1a7e8b9850

--- src/firmament/scheduler_bridge.cc
+++ src/firmament/scheduler_bridge.cc
@@ -128,8 +128,7 @@ void SchedulerBridge::CreateResourceTopologyForNode(
     ResourceStatus* pu_rs = new ResourceStatus(pu_rd_ptr, pu_rtnd_ptr, "", 0);
     CHECK(InsertIfNotPresent(resource_map_.get(), pu_rid, pu_rs));
   }
-  // TODO(malte): set hostname correctly
-  ResourceStatus* rs = new ResourceStatus(rd_ptr, rtnd_ptr, "", 0);
+  ResourceStatus* rs = new ResourceStatus(rd_ptr, rtnd_ptr, node_stats.hostname_, 0);
   // Insert into resource map
   CHECK(InsertIfNotPresent(resource_map_.get(), rid, rs));
   // Register with the scheduler
