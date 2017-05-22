5b0596320dd33988bf62a9c2d87fe61f6df92585
Ionel Gog <gogionel@gmail.com>
2017-05-23 00:25:10 +0100

    Removed deprecated run scripts.
    Change-Id: If049b42bb2c2aeca9072b1f305e68c821a025257

--- deploy/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-cp ${DIR}/../build/poseidon poseidon
-cp ${DIR}/../build/third_party/ms-cpprestsdk/src/cpp-rest-sdk-build/Binaries/libcpprest.so.2.7 libcpprest.so.2.7
-cp ${DIR}/../build/firmament/src/firmament-build/src/libfirmament_scheduling.so libfirmament_scheduling.so
-cp ${DIR}/../build/firmament/src/firmament-build/third_party/cs2/src/cs2/cs2.exe cs2.exe
-
-docker build -t camsas/poseidon:dev ${DIR}
