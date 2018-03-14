0acafb624fe176a7520532156425474c04393f2b
dhilipkumars <dhilip.kumar.s@huawei.com>
2018-03-14 20:50:23 +0530

    gofmt, govet and fix import after the project name was changed Fix BoilerPlate checkin hack/verify scripts

--- pkg/k8sclient/podwatcher.go
+++ pkg/k8sclient/podwatcher.go
@@ -1,20 +1,18 @@
-// Poseidon
-// Copyright (c) The Poseidon Authors.
-// All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
-// LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR
-// A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
-//
-// See the Apache Version 2.0 License for specific language governing
-// permissions and limitations under the License.
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package k8sclient
 
@@ -27,9 +25,10 @@ import (
 	"sync"
 	"time"
 
-	"k8s.io/scheduling_poseidon/pkg/firmament"
+	"k8s.io/poseidon/pkg/firmament"
 
 	"github.com/golang/glog"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
@@ -38,7 +37,6 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/cache"
 )
 
@@ -437,7 +435,6 @@ func GetOwnerReference(pod *v1.Pod) string {
 	if controllerID := pod.GetObjectMeta().GetLabels()["controller-uid"]; controllerID != "" {
 		return controllerID
 	}
-
 
 	// Return 'kubernetes.io/created-by' if it exists.
 	if createdByAnnotation, ok := pod.GetObjectMeta().GetAnnotations()[CreatedByAnnotation]; ok {
