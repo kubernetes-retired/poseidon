9c97c7933bbda18f437d22842cbbf89782234686
shivramsrivastava <shivram.srivastava@huawei.com>
2018-04-25 17:53:06 +0000

    Updating tests, renamed and added a new test file

--- test/e2e/poseidon_integration.go
+++ test/e2e/poseidon_integration.go
@@ -28,7 +28,6 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"testing"
 
 	"math/rand"
 	"os"
@@ -278,7 +277,7 @@ var _ = Describe("Poseidon", func() {
 
 })
 
-func TestPoseidon(t *testing.T) {
+/*func TestPoseidon(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Poseidon Suite")
-}
+}*/
